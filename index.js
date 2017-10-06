d91565069ffe538ccf30892809b8949c28a03799
Timothy Cevallos <timcev@gmail.com>
2017-10-06 02:44:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 // Code your solution here:
-function driversWithRevenueOver(driver){
-  
+function driversWithRevenueOver(driver, revenue){
+  return drivers.filter(function (driver){
+    return drivers.revenue > revenue;
+  })
 }
 
 function driverNamesWithRevenueOver(){
-  
+
 }
 
 function exactMatch(){
-  
+
 }
 
 function exactMatchToList(){
-  
+
 }
